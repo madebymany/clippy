d3ec809842b2c8d059b940f4c683a59294d5722a
Andrew Walker <andy@madebymany.co.uk>
2012-05-15 18:59:18 +0100

    first commit - just created users model with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clippy::Application.config.secret_token = '2d5cc34b69e45f7aefc337f562ec6b05f448750b11f02b830fb8402e9820c5f0b085f887ea20fbba9ccef8047d0fc422e1bd2dd72754fbe1b868e46aeaa4c324'
